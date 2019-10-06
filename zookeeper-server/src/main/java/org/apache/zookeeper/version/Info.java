48e3c8fee22d41334a290f1db28773c52532c539
鲁凤祥 <lufengxiang@nongfenqi.com>
2019-10-06 09:07:02 +0800

    add version number

---
+++ zookeeper-server/src/main/java/org/apache/zookeeper/version/Info.java
@@ -0,0 +1,14 @@
+package org.apache.zookeeper.version;
+
+/**
+ * @author lumac
+ * @since 2019-10-06
+ */
+public interface Info {
+    int MAJOR = 3;
+    int MINOR = 4;
+    int MICRO = 13;
+    String QUALIFIER = null;
+    String REVISION_HASH = "1";
+    String BUILD_DATE = "2019-10-06";
+}
